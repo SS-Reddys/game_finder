7eb97e10ea732f0ce7de655c3a9bd0eaeb3fb6b0
SS-Reddys <sujithreddysudini@gmail.com>
2024-09-23 22:41:41 -0400

    Initial commit

--- game_finder.py
+++ game_finder.py
@@ -1,12 +1,36 @@
-"""
-Given the following inputs:
-- <game_data> is a list of dictionaries, with each dictionary representing a player's shot attempts in a game. The list can be empty, but any dictionary in the list will include the following keys: gameID, playerID, gameDate, fieldGoal2Attempted, fieldGoal2Made, fieldGoal3Attempted, fieldGoal3Made, freeThrowAttempted, freeThrowMade. All values in this dictionary are ints, except for gameDate which is of type str in the format 'MM/DD/YYYY'
-- <true_shooting_cutoff> is the minimum True Shooting percentage value for a player to qualify in a game. It will be an int value >= 0.
-- <player_count> is the number of players that need to meet the <true_shooting_cutoff> in order for a gameID to qualify. It will be an int value >= 0.
-
-Implement find_qualified_games to return a list of unique qualified gameIDs in which at least <player_count> players have a True Shooting percentage >= <true_shooting_cutoff>, ordered from most to least recent game.
-"""
+from collections import defaultdict
 
 def find_qualified_games(game_data: list[dict], true_shooting_cutoff: int, player_count: int) -> list[int]:
-	# Replace the line below with your code
-	pass
+    # Function to calculate true shooting percentage for a player
+    def calculate_true_shooting(player_data):
+        # Calculate points scored
+        points = 2 * player_data['fieldGoal2Made'] + 3 * player_data['fieldGoal3Made'] + player_data['freeThrowMade']
+        # Calculate total shooting attempts
+        total_attempts = player_data['fieldGoal2Attempted'] + player_data['fieldGoal3Attempted'] + 0.44 * player_data['freeThrowAttempted']
+        if total_attempts == 0:
+            return 0
+        # Calculate TS% using the formula
+        return (points / (2 * total_attempts)) * 100
+
+    # Group player data by gameID
+    game_players = defaultdict(list)
+    game_dates = {}  # To store the date of each gameID for sorting purposes
+
+    for entry in game_data:
+        gameID = entry['gameID']
+        ts_percentage = calculate_true_shooting(entry)
+        game_players[gameID].append(ts_percentage)
+        game_dates[gameID] = entry['gameDate']
+
+    qualified_games = []
+
+    # For each game, count how many players have TS% >= true_shooting_cutoff
+    for gameID, ts_percentages in game_players.items():
+        qualified_players = sum(1 for ts in ts_percentages if ts >= true_shooting_cutoff)
+        if qualified_players >= player_count:
+            qualified_games.append(gameID)
+
+    # Sort the qualified gameIDs by most recent gameDate (descending order)
+    qualified_games.sort(key=lambda gid: game_dates[gid], reverse=True)
+
+    return qualified_games
